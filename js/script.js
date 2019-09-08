330c0a0069d7a4b6cc1db63716d906e3db3004ef
aleksandercie <45287142+aleksandercie@users.noreply.github.com>
2019-09-08 11:30:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var h = document.documentElement,
   b = document.body,
   st = 'scrollTop',
   sh = 'scrollHeight',
-  progress = document.querySelector('.progress'),
+  progress = document.getElementById('progress'),
   scroll;
 
 document.addEventListener('scroll', function() {
@@ -10,3 +10,119 @@ document.addEventListener('scroll', function() {
   progress.style.setProperty('--scroll', scroll + '%');
 });
 
+var banner = document.getElementById('banner');
+var nameBox = document.getElementById('name');
+var profession = document.getElementById('profession');
+
+TweenMax.set(banner, {visibility: "visible"});
+TweenMax.set(nameBox, {visibility: "visible"});
+TweenMax.set(profession, {visibility: "visible"});
+
+TweenMax.from(banner, 5, {opacity: 0})
+TweenMax.from(nameBox, 3, {opacity: 0, delay: 0.7});
+TweenMax.from(profession, 3, {opacity: 0, delay: 1.2});
+
+var positionAboutElement = -(window.innerWidth);
+
+jQuery.noConflict();
+jQuery(document).ready(function() {
+    		var controller = new ScrollMagic.Controller();
+    		var blockTween = new TweenMax.from('.photo-about', 3, {
+    			left: positionAboutElement
+    		});
+    		var containerScene = new ScrollMagic.Scene({
+    			triggerElement: '.person',
+
+                
+                offset: -200, 
+                duration: 250 
+                
+
+    		})
+    		.setTween(blockTween)
+    		.addTo(controller);
+            
+    })
+    
+    jQuery.noConflict();
+    jQuery(document).ready(function() {
+            var controller = new ScrollMagic.Controller();
+            var blockTween = new TweenMax.from('.text', 3, {
+              right: positionAboutElement
+            });
+            var containerScene = new ScrollMagic.Scene({
+              triggerElement: '.person',
+    
+                    
+                    offset: -350, 
+                    duration: 200 
+                    
+    
+            })
+            .setTween(blockTween)
+            .addTo(controller);
+                
+        })
+
+        jQuery.noConflict();
+        jQuery(document).ready(function() {
+                var controller = new ScrollMagic.Controller();
+                var blockTween = new TweenMax.from('.examples', 5, {
+                  backgroundColor: '#4c4c4c',
+                  color: '#ffffff'
+                  
+                });
+                var containerScene = new ScrollMagic.Scene({
+                  triggerElement: '#projects',
+        
+                        
+                        offset: -200, 
+                        duration: 400 
+                        
+        
+                })
+                .setTween(blockTween)
+                .addTo(controller);
+                    
+            })
+
+        jQuery.noConflict();
+        jQuery(document).ready(function() {
+                var controller = new ScrollMagic.Controller();
+                var blockTween = new TweenMax.from('.contact', 5, {
+                  backgroundColor: '#ffffff'
+                  
+                });
+                var containerScene = new ScrollMagic.Scene({
+                  triggerElement: '.contact',
+        
+                        
+                        offset: -100, 
+                        duration: 400 
+                        
+        
+                })
+                .setTween(blockTween)
+                .addTo(controller);
+                    
+            })
+            jQuery.noConflict();
+            jQuery(document).ready(function() {
+                    var controller = new ScrollMagic.Controller();
+                    var blockTween = new TweenMax.from('.form', 5, {
+                      opacity: 0
+                      
+                    });
+                    var containerScene = new ScrollMagic.Scene({
+                      triggerElement: '.contact',
+            
+                            
+                            offset: -100, 
+                            duration: 400 
+                            
+            
+                    })
+                    .setTween(blockTween)
+                    .addTo(controller);
+                        
+                })
